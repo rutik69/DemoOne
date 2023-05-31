327b71c870f3aef40b16acb1681b60cec6f255c8
rutik <rutikmaraskolhe884@gmail.com>
2023-05-31 14:41:56 +0530

    create Projectt

---
+++ DemoOne/DemoOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoOneApp.swift
+//  DemoOne
+//
+//  Created by Dr. Shrikant Maraskolhe on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
